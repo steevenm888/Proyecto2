4934597bd9e3f654ca0db82c659edefd2e4a1aae
steevenm888 <egmolina4@espe.edu.ec>
2018-10-10 23:22:16 -0500

    Implementacion de metodos

--- main.cpp
+++ main.cpp
@@ -8,5 +8,8 @@ int main(int argc, char** argv) {
 	Suma<float> obj(200, 999.99);
 	Suma<int> obj1(9, 6);
 	Suma<double> obj2(2.89779879, 3.28931746);
+	cout<<"La suma entre dos numeros flotantes es: "<<obj.sumaNumeros()<<" la resta es: "<<obj.restaNumeros()<<" y la division: "<<obj.divisionNumeros()<<endl;
+	cout<<"La suma entre dos numeros enteros es: "<<obj1.sumaNumeros()<<" la resta es: "<<obj1.restaNumeros()<<" y la division: "<<obj1.divisionNumeros()<<endl;
+	cout<<"La suma entre dos numeros doubles es: "<<obj2.sumaNumeros()<<" la resta es: "<<obj2.restaNumeros()<<" y la division: "<<obj2.divisionNumeros()<<endl;
 	return 0;
 }
